4447b08572f96722ceb4e30c47c9935f2e3bcf5c
shubham <shubhamv430@gmail.com>
2021-09-07 00:50:50 +0530

    firebase setup

--- src/Redux/auth/reducer.js
+++ src/Redux/auth/reducer.js
@@ -14,7 +14,6 @@ export const authReducer = (state=initState,{type,payload})=>{
             }
         }
         case SIGN_UP_SUCCESS:{
-            saveData("token",payload);
             return {
                ...state,
                isAuth:true,
